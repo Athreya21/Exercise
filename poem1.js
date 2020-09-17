d8902f4e88b9aa2ae4d5b8e4c7ef3da5d607e8b5
Athreya21 <athreya.sr@robosoftin.com>
2020-09-17 13:05:22 +0530

    Poem1 prog

---
+++ poem1.js
@@ -0,0 +1,8 @@
+"use strict";
+let i;
+for (i = 99; i > 0; i--) {
+    console.log(i + " bottles of water on the wall, \n" + i + " bottles of water,\n" + " ya’ take one down, ya’ pass it around,");
+}
+if (i === 0) {
+    console.log("No bottles of water on the wall,\n" + "no bottles of water\n" + " ya’ can’t take one down, ya’ can’t pass it around,\n" + "’cause there are no more bottles of water on the wall!");
+} 
